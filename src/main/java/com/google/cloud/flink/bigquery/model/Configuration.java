2ef545f59a7501a3e45852f60c535ff45a80a8bf
sridhar durgam <sridhar.durgam@quantiphi.com>
2022-03-17 14:09:35 +0530

    Flink-BQ connector - Added support for Arrow format - Addressed review comments

--- src/main/java/com/google/cloud/flink/bigquery/model/Configuration.java
+++ src/main/java/com/google/cloud/flink/bigquery/model/Configuration.java
@@ -19,103 +19,122 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Configuration {
-	
-	private String projectId;
-	private String dataset;
-	private String table;
-	private String filter="";
-	private String credentialKeyFile="";	
-	private String selectedfields="";
-	private int DEFAULT_PARALLELISM=5;
-	private String FLINK_VERSION="";
-	private int snapshotMillis=0;
-	private Map<String,String> configMap = new HashMap<>(); 
-	private String targetTable="";
-	private String configString	="";
-	private String partitionRequired=""; //0 = true, 1 = false
-	private String partitionField="";	//0 = true, 1 = false
-	public String getTargetTable() {
-		return targetTable;
+
+	private static String project_id = "";
+	private static String dataset = "";
+	private static String bigQueryReadTable = "";
+	private static String filter = "";
+	private static String credentialKeyFile = "";
+	private static String credentialKeys = "";
+	private static String selectedfields = "";
+	private static int defaultParallelism = 5;
+	private static String flinkVersion = "";
+	private static int snapshotMillis = 0;
+	private static Map<String, String> configMap = new HashMap<>();
+	private static String bigQueryWriteTable = "";
+	private static String configString = "";
+	private static String partitionRequired = ""; // 0 = true, 1 = false
+	private static String partitionField = "";
+	private static String bqEncodedCreateReadSessionRequest = "";
+	private static int bqBackgroundThreadsPerStream = 1;
+	private static String query;
+	private static String path;
+	// 0 = true, 1 = false
+
+	public static String getPath() {
+		return path;
 	}
 
-	public void setTargetTable(String targetTable) {
-		this.targetTable = targetTable;
-		configMap.put("targetTable",targetTable);
+	public static void setPath(String path) {
+		Configuration.path = path;
+	}
+
+	public String getBigQueryWriteTable() {
+		return bigQueryWriteTable;
+	}
+
+	public void setBigQueryWriteTable(String targetBigQueryTable) {
+		this.bigQueryWriteTable = targetBigQueryTable;
 	}
 
 	public void setSnapshotMillis(int snapshotMillis) {
 		this.snapshotMillis = snapshotMillis;
-		configMap.put("snapshotMillis", String.valueOf(snapshotMillis));
 	}
-	
+
 	public String getProjectId() {
-		return projectId;
+		return project_id;
 	}
-	public void setProjectId(String projectId) {
-		this.projectId = projectId;
-		configMap.put("project_id", projectId);
+
+	public void setProjectId(String project_id) {
+		this.project_id = project_id;
 	}
+
 	public String getDataset() {
 		return dataset;
 	}
+
 	public void setDataset(String dataset) {
 		this.dataset = dataset;
-		configMap.put("dataset", dataset);
 	}
-	public String getTable() {
-		return table;
+
+	public String getBigQueryReadTable() {
+		return bigQueryReadTable;
 	}
-	public void setTable(String table) {
-		this.table = table;
-		configMap.put("table", table);
+
+	public void setBigQueryReadTable(String bigQueryTable) {
+		this.bigQueryReadTable = bigQueryTable;
 	}
+
 	public String getFilter() {
 		return filter;
 	}
+
 	public void setFilter(String filter) {
 		this.filter = filter;
-		configMap.put("filter", filter);
 	}
+
 	public String getGcpCredentialKeyFile() {
 		return credentialKeyFile;
 	}
+
 	public void setGcpCredentialKeyFile(String credentialKFile) {
 		this.credentialKeyFile = credentialKFile;
-		configMap.put("credentialKeyFile", credentialKFile);
 	}
+
 	public String getSelectedFields() {
 		return selectedfields;
 	}
+
 	public void setSelectedFields(String fields) {
 		this.selectedfields = fields;
-		configMap.put("selectedfields", fields);
-	}
-	public int getDEFAULT_PARALLELISM() {
-		return DEFAULT_PARALLELISM;
-	}
-	public void setDEFAULT_PARALLELISM(int dEFAULT_PARALLELISM) {
-		DEFAULT_PARALLELISM = dEFAULT_PARALLELISM;
-		configMap.put("DEFAULT_PARALLELISM", String.valueOf(dEFAULT_PARALLELISM));
 	}
 
-	public String getFLINK_VERSION() {
-		return FLINK_VERSION;
+	public int getDefaultParallelism() {
+		return defaultParallelism;
 	}
-	public void setFLINK_VERSION(String fLINK_VERSION) {
-		FLINK_VERSION = fLINK_VERSION;
-		configMap.put("FLINK_VERSION", fLINK_VERSION);
+
+	public void setDefaultParallelism(int defaultParallelism) {
+		this.defaultParallelism = defaultParallelism;
 	}
+
+	public String getFlinkVersion() {
+		return flinkVersion;
+	}
+
+	public void setFlinkVersion(String flinkVersion) {
+		this.flinkVersion = flinkVersion;
+	}
+
 	public int getSnapshotMillis() {
 		return snapshotMillis;
-	}	
-	
+	}
+
 	public String getPartitionRequired() {
 		return partitionRequired;
 	}
 
 	public void setPartitionRequired(String partitionRequired) {
 		this.partitionRequired = partitionRequired;
-		configMap.put("partitionRequired", partitionRequired);
 	}
 
 	public String getPartitionField() {
@@ -124,22 +143,50 @@ public class Configuration {
 
 	public void setPartitionField(String partitionField) {
 		this.partitionField = partitionField;
-		configMap.put("partitionField", partitionField);
 	}
-	
+
+	public String getBqEncodedCreateReadSessionRequest() {
+		return bqEncodedCreateReadSessionRequest;
+	}
+
+	public void setBqEncodedCreateReadSessionRequest(String bqEncodedCreateReadSessionRequest) {
+		this.bqEncodedCreateReadSessionRequest = bqEncodedCreateReadSessionRequest;
+	}
+
+	public int getBqBackgroundThreadsPerStream() {
+		return bqBackgroundThreadsPerStream;
+	}
+
+	public void setBqBackgroundThreadsPerStream(int bqBackgroundThreadsPerStream) {
+		this.bqBackgroundThreadsPerStream = bqBackgroundThreadsPerStream;
+	}
+
+	public String getQuery() {
+		return query;
+	}
+
+	public void setQuery(String query) {
+		this.query = query;
+	}
+
 	public String getConfigMap() {
-		this.configString=this.configString+"targetTable="+targetTable+"#";
-		this.configString=this.configString+"snapshotMillis="+String.valueOf(snapshotMillis)+"#";
-		this.configString=this.configString+"project_id="+projectId+"#";
-		this.configString=this.configString+"dataset="+dataset+"#";
-		this.configString=this.configString+"table="+table+"#";
-		this.configString=this.configString+"filter="+filter+"#";
-		this.configString=this.configString+"credentialKeyFile="+credentialKeyFile+"#";
-		this.configString=this.configString+"selectedfields="+selectedfields+"#";
-		this.configString=this.configString+"DEFAULT_PARALLELISM="+String.valueOf(DEFAULT_PARALLELISM)+"#";
-		this.configString=this.configString+"FLINK_VERSION="+FLINK_VERSION+"#";
-		this.configString=this.configString+"partitionRequired="+partitionRequired+"#";
-		this.configString=this.configString+"partitionField="+partitionField+"#";
-		return this.configString.substring(0, this.configString.length() -1);
+		this.configString = this.configString + "targetBigQueryTable::" + bigQueryWriteTable + "#";
+		this.configString = this.configString + "snapshotMillis::" + String.valueOf(snapshotMillis) + "#";
+		this.configString = this.configString + "project_id::" + project_id + "#";
+		this.configString = this.configString + "dataset::" + dataset + "#";
+		this.configString = this.configString + "table::" + bigQueryReadTable + "#";
+		this.configString = this.configString + "filter::" + filter + "#";
+		this.configString = this.configString + "credentialKeyFile::" + credentialKeyFile + "#";
+		this.configString = this.configString + "selectedfields::" + selectedfields + "#";
+		this.configString = this.configString + "defaultParallelism::" + String.valueOf(defaultParallelism) + "#";
+		this.configString = this.configString + "flinkVersion::" + flinkVersion + "#";
+		this.configString = this.configString + "partitionRequired::" + partitionRequired + "#";
+		this.configString = this.configString + "partitionField::" + partitionField + "#";
+		this.configString = this.configString + "bqBackgroundThreadsPerStream::" + bqBackgroundThreadsPerStream + "#";
+		this.configString = this.configString + "bqEncodedCreateReadSessionRequest::"
+				+ bqEncodedCreateReadSessionRequest + "#";
+		this.configString = this.configString + "query::" + query + "#";
+		this.configString = this.configString + "path::" + path + "#";
+		return this.configString.substring(0, this.configString.length() - 1);
 	}
 }
