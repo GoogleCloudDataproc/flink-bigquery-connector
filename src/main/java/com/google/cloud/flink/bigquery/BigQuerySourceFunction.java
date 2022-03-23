39452a7c7a512055dd9fd5cdf95aa8b6dea41c3f
David Rabinowitz <davidrab@google.com>
2022-03-23 09:45:54 -0700

    spotless applied

--- src/main/java/com/google/cloud/flink/bigquery/BigQuerySourceFunction.java
+++ src/main/java/com/google/cloud/flink/bigquery/BigQuerySourceFunction.java
@@ -15,7 +15,6 @@
  */
 package com.google.cloud.flink.bigquery;
 
-<<<<<<< HEAD
 import com.google.cloud.bigquery.connector.common.BigQueryClientFactory;
 import com.google.cloud.bigquery.connector.common.ReadRowsHelper;
 import com.google.cloud.bigquery.connector.common.ReadRowsHelper.Options;
@@ -141,53 +140,4 @@ public final class BigQuerySourceFunction extends RichParallelSourceFunction<Row
       // no op as we don't want to close ctx
     }
   };
-=======
-import org.apache.flink.api.common.serialization.DeserializationSchema;
-import org.apache.flink.api.common.serialization.RuntimeContextInitializationContextAdapters;
-import org.apache.flink.api.common.typeinfo.TypeInformation;
-import org.apache.flink.api.java.typeutils.ResultTypeQueryable;
-import org.apache.flink.configuration.Configuration;
-import org.apache.flink.streaming.api.functions.source.RichSourceFunction;
-import org.apache.flink.table.data.RowData;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.google.cloud.bigquery.storage.v1.BigQueryReadClient;
-import com.google.cloud.bigquery.storage.v1.ReadSession;
-
-public final class BigQuerySourceFunction extends RichSourceFunction<RowData> implements ResultTypeQueryable<RowData> {
-
-	private static final long serialVersionUID = 1;
-	private static final Logger log = LoggerFactory.getLogger(BigQuerySourceFunction.class);	
-
-	private final DeserializationSchema<RowData> deserializer;
-
-	public BigQuerySourceFunction(DeserializationSchema<RowData> deserializer) {
-		this.deserializer = deserializer;
-	}
-
-	@Override
-	public TypeInformation<RowData> getProducedType() {
-		return deserializer.getProducedType();
-	}
-
-	@Override
-	public void open(Configuration parameters) throws Exception {
-		deserializer.open(RuntimeContextInitializationContextAdapters.deserializationAdapter(getRuntimeContext()));
-	}
-
-	@Override
-	public void run(SourceContext<RowData> ctx) throws Exception {
-
-		BigQueryReadClient client = BigQueryReadClient.create();
-		ReadSession readSession = BigQueryDynamicTableFactory.readSession;
-		String streamName = readSession.getStreams(0).getName();
-		ReadRows readRows = new ReadRows();
-		readRows.createReadRowRequest(deserializer, streamName, client, ctx);
-	}
-
-	@Override
-	public void cancel() {
-	}
->>>>>>> 63846ce (Flink big query - Arrow format support)
 }
