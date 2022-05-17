177c3e972e38e0292897781b365f1a9c2611f447
sridhar durgam <sridhar.durgam@quantiphi.com>
2022-05-17 16:55:45 +0530

    unit test cases added

---
+++ src/test/java/com/google/cloud/flink/bigquery/BigQueryDynamicTableSourceTest.java
@@ -0,0 +1,174 @@
+/*
+ * Copyright 2020 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.cloud.flink.bigquery;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.cloud.bigquery.connector.common.BigQueryClientFactory;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.apache.flink.api.common.serialization.DeserializationSchema;
+import org.apache.flink.configuration.ConfigOption;
+import org.apache.flink.configuration.ConfigOptions;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.table.api.TableSchema;
+import org.apache.flink.table.catalog.CatalogTable;
+import org.apache.flink.table.catalog.CatalogTableImpl;
+import org.apache.flink.table.catalog.ObjectIdentifier;
+import org.apache.flink.table.connector.ChangelogMode;
+import org.apache.flink.table.connector.format.DecodingFormat;
+import org.apache.flink.table.connector.source.DynamicTableSource;
+import org.apache.flink.table.connector.source.ScanTableSource;
+import org.apache.flink.table.connector.source.ScanTableSource.ScanRuntimeProvider;
+import org.apache.flink.table.connector.source.SourceFunctionProvider;
+import org.apache.flink.table.data.RowData;
+import org.apache.flink.table.descriptors.DescriptorProperties;
+import org.apache.flink.table.factories.FactoryUtil;
+import org.apache.flink.table.types.DataType;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class BigQueryDynamicTableSourceTest {
+
+  static String bigqueryReadTable;
+  static ObjectIdentifier tableIdentifier;
+  static TableSchema tableSchema;
+  static Configuration options = new Configuration();
+
+  @BeforeClass
+  public static void setup() {
+    bigqueryReadTable = "q-gcp-6750-pso-gs-flink-22-01.wordcount_dataset.wordcount_output";
+  }
+
+  private DynamicTableSource getTableSource(String tableName) throws Exception {
+
+    MockDynamicTableContext contextObj = createContextObject();
+
+    BigQueryDynamicTableFactory dynamicTableFactory = new BigQueryDynamicTableFactory();
+    BigQueryDynamicTableSource dynamicTableSource =
+        (BigQueryDynamicTableSource) dynamicTableFactory.createDynamicTableSource(contextObj);
+    return dynamicTableSource;
+  }
+
+  @Test
+  public void getScanRuntimeProviderSuccessfulTest() {
+
+    // Mock the big query client factory
+    BigQueryClientFactory mockBigQueryClientFactory = Mockito.mock(BigQueryClientFactory.class);
+
+    ArrowFormatFactory arrowFormat = new ArrowFormatFactory();
+    DataType producedDataType =
+        createContextObject().getCatalogTable().getSchema().toPhysicalRowDataType();
+
+    DecodingFormat<DeserializationSchema<RowData>> decodingFormat =
+        arrowFormat.createDecodingFormat(createContextObject(), options);
+
+    ArrayList<String> readStreamNames = new ArrayList<String>();
+
+    // Initialize the constructor
+    BigQueryDynamicTableSource bigQueryDynamicTableSource =
+        new BigQueryDynamicTableSource(
+            decodingFormat, producedDataType, readStreamNames, mockBigQueryClientFactory);
+
+    ScanRuntimeProvider resultScanRuntimeProvider =
+        bigQueryDynamicTableSource.getScanRuntimeProvider(new MockScanContext());
+
+    assertThat(bigQueryDynamicTableSource instanceof BigQueryDynamicTableSource);
+    assertThat(bigQueryDynamicTableSource.getChangelogMode()).isNotNull();
+    assertThat(bigQueryDynamicTableSource.getChangelogMode() instanceof ChangelogMode);
+    assertThat(bigQueryDynamicTableSource.copy() instanceof BigQueryDynamicTableSource);
+    assertThat(bigQueryDynamicTableSource.asSummaryString()).isEqualTo("BigQuery Table Source");
+    assertThat(resultScanRuntimeProvider).isNotNull();
+  }
+
+  @Test
+  public void testSourceFunction() throws Exception {
+
+    DynamicTableSource dynamicTableSource = getTableSource("bigquery");
+
+    // Test the TableSource Class
+    ScanTableSource.ScanRuntimeProvider scanRuntimeProvider =
+        ((BigQueryDynamicTableSource) dynamicTableSource)
+            .getScanRuntimeProvider(new MockScanContext());
+
+    assertThat(scanRuntimeProvider).isNotNull();
+    assertThat(scanRuntimeProvider.isBounded()).isEqualTo(false);
+    assertThat(
+        ((BigQueryDynamicTableSource) dynamicTableSource).copy()
+            instanceof BigQueryDynamicTableSource);
+
+    BigQuerySourceFunction bqSourceFunction =
+        (BigQuerySourceFunction)
+            ((SourceFunctionProvider) scanRuntimeProvider).createSourceFunction();
+    assertThat(bqSourceFunction).isNotNull();
+    assertThat(dynamicTableSource.asSummaryString()).isEqualTo("BigQuery Table Source");
+  }
+
+  private MockDynamicTableContext createContextObject() {
+
+    ObjectIdentifier tableIdentifier = ObjectIdentifier.of("csvcatalog", "default", "csvtable");
+
+    List<String> partitionColumnList = new ArrayList<String>();
+    DescriptorProperties tableSchemaProps = new DescriptorProperties(true);
+
+    TableSchema tableSchema =
+        tableSchemaProps
+            .getOptionalTableSchema("Schema")
+            .orElseGet(
+                () ->
+                    tableSchemaProps
+                        .getOptionalTableSchema("generic.table.schema")
+                        .orElseGet(() -> TableSchema.builder().build()));
+    Map<String, String> configOptions = new HashMap<>();
+    configOptions.put("table", bigqueryReadTable);
+    configOptions.put(FactoryUtil.FORMAT.key(), "arrow");
+    configOptions.put(FactoryUtil.CONNECTOR.key(), "bigquery");
+    configOptions.put("selectedFields", "word,word_count");
+
+    ConfigOption<String> table = ConfigOptions.key("table").stringType().noDefaultValue();
+    ConfigOption<String> query = ConfigOptions.key("query").stringType().noDefaultValue();
+    ConfigOption<String> filter = ConfigOptions.key("filter").stringType().defaultValue("");
+    ConfigOption<String> format = ConfigOptions.key("format").stringType().defaultValue("");
+    ConfigOption<String> selected_fields =
+        ConfigOptions.key("selectedFields").stringType().noDefaultValue();
+
+    options.set(table, bigqueryReadTable);
+    options.set(format, "arrow");
+    options.set(query, "select word,word_count from table");
+    options.set(filter, "word_count>100");
+    options.set(selected_fields, "word,word_count");
+
+    CatalogTable catalogTable =
+        (CatalogTable)
+            new CatalogTableImpl(
+                tableSchema, partitionColumnList, configOptions, "sample table creation");
+
+    CatalogTableImpl resolvedCatalogTable =
+        new CatalogTableImpl(catalogTable.getSchema(), configOptions, "comments for table");
+
+    ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+
+    // Create the context object
+    MockDynamicTableContext contextObj =
+        new MockDynamicTableContext(
+            tableIdentifier, resolvedCatalogTable, configOptions, options, classloader, false);
+
+    return contextObj;
+  }
+}
