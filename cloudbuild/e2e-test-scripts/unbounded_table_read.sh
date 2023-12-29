ce889cb8158cd1c3c2f5f6a89e270d73aa3ae9a9
Prashasti Agarwal <prashastia@google.com>
2023-12-29 23:31:01 +0530

    Changes the shell file to accept command line arguments.

--- cloudbuild/e2e-test-scripts/unbounded_table_read.sh
+++ cloudbuild/e2e-test-scripts/unbounded_table_read.sh
@@ -22,13 +22,13 @@ now=$(date "+%Y-%m-%d")
 bq rm -t -f "$PROJECT_NAME":"$DATASET_NAME"."$TABLE_NAME"
 
 # Create a new partitioned table and insert initial values.
-python3 cloudbuild/python-scripts/create_partitioned_table.py -- --now_timestamp="$now" --project_name="$PROJECT_NAME" --dataset_name="$DATASET_NAME" --table_name="$TABLE_NAME"
+python3 cloudbuild/python-scripts/create_partitioned_table.py -- --now_timestamp "$now" --project_name "$PROJECT_NAME" --dataset_name "$DATASET_NAME" --table_name "$TABLE_NAME"
 
 # Running this job async to make sure it exits so that dynamic data can be added
 gcloud dataproc jobs submit flink --id "$JOB_ID" --jar="$GCS_JAR_LOCATION" --cluster="$CLUSTER_NAME" --region="$REGION" --async -- --gcp-project "$PROJECT_NAME" --bq-dataset "$DATASET_NAME" --bq-table "$TABLE_NAME" --mode unbounded --agg-prop "$AGG_PROP_NAME" --ts-prop "$TS_PROP_NAME" --partition-discovery-interval "$PARTITION_DISCOVERY_INTERVAL"
 
 # Dynamically adding the data. This is timed 2.5 min wait for read and 5 min refresh time.
-python3 cloudbuild/python-scripts/insert_dynamic_partitions.py -- --now_timestamp="$now" --project_name="$PROJECT_NAME" --dataset_name="$DATASET_NAME" --table_name="$TABLE_NAME" --refresh_interval="$PARTITION_DISCOVERY_INTERVAL"
+python3 cloudbuild/python-scripts/insert_dynamic_partitions.py -- --now_timestamp "$now" --project_name "$PROJECT_NAME" --dataset_name "$DATASET_NAME" --table_name "$TABLE_NAME" --refresh_interval "$PARTITION_DISCOVERY_INTERVAL"
 
 # Wait for a bit, as mapping and output of records takes some time.
 sleep 3m
