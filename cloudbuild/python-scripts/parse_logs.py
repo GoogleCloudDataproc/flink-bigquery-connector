f344f5dc6336fc451990a26d57cccd1c13efa50a
Prashasti Agarwal <prashastia@google.com>
2023-12-29 20:16:03 +0530

    Adds argparse - for trial

--- cloudbuild/python-scripts/parse_logs.py
+++ cloudbuild/python-scripts/parse_logs.py
@@ -17,11 +17,11 @@
 This python file extracts the status of a dataproc job from its job-id and then
 uses this job-id to get the yarn application number.
 The yarn application number enables it to read through the yarn logs to check
-for the number iof records.
+for the number of records.
 In case the number of records match that of BQ table it returns, in case of
 mismatch, it throws an error.
 """
-
+import string
 from collections.abc import Sequence
 import re
 
@@ -30,7 +30,7 @@ from absl import logging
 from google.cloud import bigquery
 from google.cloud import dataproc_v1
 from google.cloud import storage
-from utils import utils
+import argparse
 
 
 def get_bq_query_result_row_count(client_project_name, query):
@@ -415,34 +415,44 @@ def run(
         raise AssertionError('Rows do not match')
 
 
-def main(argv: Sequence[str]) -> None:
-    acceptable_arguments = {
-        'job_id',
-        'project_id',
-        'cluster_name',
-        'region',
-        'project_name',
-        'dataset_name',
-        'table_name',
-        'query',
-    }
-    required_arguments = acceptable_arguments - {'query'}
+def main() -> None:
 
-    # Makes use of the ArgumentInputUtils for input.
-    arg_input_utils = utils.ArgumentInputUtils(
-        argv, required_arguments, acceptable_arguments
-    )
-    arguments_dictionary = arg_input_utils.input_validate_and_return_arguments()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--job_id', dest='job_id', help='Job ID of the dataproc job.',
+                        type=str, required=True)
+    parser.add_argument('--project_id', dest='project_id', help='Project ID of the project '
+                                                                'containing the cluster job.',
+                        type=str, required=True)
+    parser.add_argument('--cluster_name', dest='cluster_name', help='Name of the cluster which '
+                                                                    'runs the dataproc job.',
+                        type=str, required=True)
+    parser.add_argument('--region', dest='region', help='Region of the cluster which runs the '
+                                                        'dataproc job.',
+                        type=str, required=True)
+
+    # Note: When Query is provided- project_name, dataset_name and table name are not required.
+    parser.add_argument('--project_name', dest='project_name', help='Project Id which contains '
+                                                                    'the table to be read.',
+                        type=str, default='', required=False)
+    parser.add_argument('--dataset_name', dest='dataset_name', help='Dataset Name which contains '
+                                                                    'the table to be read.',
+                        type=str, default='', required=False)
+    parser.add_argument('--table_name', dest='table_name', help='Table Name of the table which is '
+                                                                'read in the test.',
+                        type=str, default='', required=False)
+    parser.add_argument('query', dest='query', help='Query to be executed (if any)',
+                        default='', type=str, required=False)
+    args = parser.parse_args()
 
     # Providing the values.
-    job_id = arguments_dictionary['job_id']
-    project_id = arguments_dictionary['project_id']
-    cluster_name = arguments_dictionary['cluster_name']
-    region = arguments_dictionary['region']
-    project_name = arguments_dictionary['project_name']
-    dataset_name = arguments_dictionary['dataset_name']
-    table_name = arguments_dictionary['table_name']
-    query = arguments_dictionary.get('query', '')
+    job_id = args.job_id
+    project_id = args.project_id
+    cluster_name = args.cluster_name
+    region = args.region
+    project_name = args.project_name
+    dataset_name = args.dataset_name
+    table_name = args.table_name
+    query = args.query
 
     run(
         project_id,
--- cloudbuild/python-scripts/utils/utils.py
+++ cloudbuild/python-scripts/utils/utils.py
@@ -9,6 +9,8 @@ import avro
 import avro.datafile
 import avro.io
 from google.cloud import bigquery
+import argparse
+
 
 
 def is_perfect_hour(datetime_obj):
@@ -185,65 +187,3 @@ class TableCreationUtils:
         )
         self.transfer_avro_rows_to_bq_table(avro_file_local_identifier)
         self.delete_local_file(avro_file_local_identifier)
-
-
-class ArgumentInputUtils:
-    """Class for command like argument intake and validation."""
-
-    def __init__(
-        self,
-        argv,
-        required_arguments,
-        acceptable_arguments,
-    ):
-        self.argv = argv
-        self.required_arguments = required_arguments
-        self.acceptable_arguments = acceptable_arguments
-
-    def _get_arguments(self):
-        """Parse the command line arguments and store them in a dictionary.
-
-        Returns:
-          A dictionary {argument_name: argument_value}.
-            argument_name: The name of the argument provided.
-            argument_value: The value for the concerned named argument.
-        """
-        # Arguments are provided of the form "--argument_name=argument_value"
-        # We need to extract the name and value as a part of a dictionary.
-        # i.e
-        # {argument1_name: argument1_value, argument2_name: argument2_value, ...}
-        # containing all arguments
-        # The pattern
-        #     --(\w+)=(.*): Searches for the exact '--'
-        #         followed by a group of word character (alphanumeric & underscore)
-        #         then an '=' sign
-        #         followed by any character except linebreaks
-        argument_pattern = r'--(\w+)=(.*)'
-
-        # Forming a dictionary from the arguments
-        try:
-            matches = [
-                re.match(argument_pattern, argument) for argument in self.argv[1:]
-            ]
-            argument_dictionary = {
-                match.group(1): match.group(2) for match in matches
-            }
-            del matches
-        except AttributeError as exc:
-            raise UserWarning(
-                'Missing argument. Please check the arguments provided again.'
-            ) from exc
-        return argument_dictionary
-
-    def _validate_arguments(self, arguments_dictionary):
-        for required_argument in self.required_arguments:
-            if required_argument not in arguments_dictionary:
-                raise UserWarning(f'{required_argument} argument not provided')
-        for key, _ in arguments_dictionary.items():
-            if key not in self.acceptable_arguments:
-                raise UserWarning(f'Invalid argument "{key}" provided')
-
-    def input_validate_and_return_arguments(self):
-        arguments_dictionary = self._get_arguments()
-        self._validate_arguments(arguments_dictionary)
-        return arguments_dictionary
