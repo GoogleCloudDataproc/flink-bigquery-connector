d48c34551029175682aeb539f4c645741f4e17ef
Prashasti Agarwal <prashastia@google.com>
2024-01-03 15:09:51 +0530

    Addresses review comments.

--- cloudbuild/python-scripts/insert_dynamic_partitions.py
+++ cloudbuild/python-scripts/insert_dynamic_partitions.py
@@ -98,14 +98,14 @@ def main(argv: Sequence[str]) -> None:
         table_id,
     )
 
-    # Insert in iterations.
+    # Insert iteratively.
     prev_partitions_offset = 0
     for number_of_partitions in partitions:
         start_time = time.time()
         # Wait for read stream formation.
         wait()
 
-        # This is an iteration of insertion.
+        # This represents one iteration.
         for partition_number in range(number_of_partitions):
             threads = list()
             # Insert via concurrent threads.
