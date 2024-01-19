13df2931fcb65e232bc3f771daa51907b284ce19
Prashasti Agarwal <prashastia@google.com>
2024-01-19 14:29:15 +0530

    Removes retries as wait time for searching logs has been increased.

--- cloudbuild/python-scripts/parse_logs.py
+++ cloudbuild/python-scripts/parse_logs.py
@@ -371,7 +371,6 @@ def read_logs(cluster_temp_bucket, logs_pattern, query, mode):
     # the pattern enables searching for the same.
     logs_pattern = re.compile(rf'{re.escape(logs_pattern)}/.+')
 
-    wait_time = 5
     # Get all the contents in the GCS Bucket.
     gcs_bucket_contents = get_bucket_contents(cluster_temp_bucket)
     # Find all strings in the array that match the pattern.
@@ -380,20 +379,6 @@ def read_logs(cluster_temp_bucket, logs_pattern, query, mode):
         for gcs_log_object in gcs_bucket_contents
         if logs_pattern.match(gcs_log_object)
     ]
-    #  If the logs have not been found yet.
-    # Retry to make sure the logs are actually absent.
-    no_retries = 5
-    while len(gcs_log_objects) == 0 and no_retries > 0:
-        logging.info('Attempting to find log files again.')
-        time.sleep(wait_time)
-        # Get all the contents in the GCS Bucket.
-        gcs_bucket_contents = get_bucket_contents(cluster_temp_bucket)
-        # Find all strings in the array that match the pattern.
-        gcs_log_objects = [
-            gcs_log_object
-            for gcs_log_object in gcs_bucket_contents
-            if logs_pattern.match(gcs_log_object)
-        ]
 
     for gcs_log_object in gcs_log_objects:
         logging.info(f'Found logs file: {gcs_log_object}')
