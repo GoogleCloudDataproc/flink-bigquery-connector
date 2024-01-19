eff7cf249323a95ee3a1f59cca6fc16c923fe0a0
Prashasti Agarwal <prashastia@google.com>
2024-01-19 22:20:40 +0530

    Removes retries for duplicate job id creation.

--- cloudbuild/e2e-test-scripts/bounded_table_read.sh
+++ cloudbuild/e2e-test-scripts/bounded_table_read.sh
@@ -14,18 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Adds retries in case same job-id has been randomly generated again.
-# Retry the command until it succeeds or maximum 5 times.
-# Delay of 5 seconds before retrying.
-retries=5
-delay=5
-for _ in $(seq 1 $retries); do
-  # We won't run this async as we can wait for a bounded job to succeed or fail.
-  gcloud dataproc jobs submit flink --id "$JOB_ID" --jar="$GCS_JAR_LOCATION" --cluster="$CLUSTER_NAME" --region="$REGION" -- --gcp-project "$PROJECT_NAME" --bq-dataset "$DATASET_NAME" --bq-table "$TABLE_NAME" --agg-prop "$AGG_PROP_NAME" --query "$QUERY_STRING"
-  if [[ $? -eq 0 ]]; then
-    break
-  fi
-  sleep $delay
-  JOB_ID=$(echo "$RANDOM" | md5sum | cut -c 1-30)
-done
+# We won't run this async as we can wait for a bounded job to succeed or fail.
+gcloud dataproc jobs submit flink --id "$JOB_ID" --jar="$GCS_JAR_LOCATION" --cluster="$CLUSTER_NAME" --region="$REGION" -- --gcp-project "$PROJECT_NAME" --bq-dataset "$DATASET_NAME" --bq-table "$TABLE_NAME" --agg-prop "$AGG_PROP_NAME" --query "$QUERY_STRING"
 
