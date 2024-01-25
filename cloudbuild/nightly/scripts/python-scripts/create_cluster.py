f5abb4f089151cd7e4bafe89269498212268f581
Prashasti Agarwal <prashastia@google.com>
2024-01-25 12:14:55 +0530

    Fixes the name issue.

--- cloudbuild/nightly/scripts/python-scripts/create_cluster.py
+++ cloudbuild/nightly/scripts/python-scripts/create_cluster.py
@@ -16,7 +16,7 @@ import argparse
 from absl import logging
 from absl import app
 from collections.abc import Sequence
-
+import os
 from google.cloud.storage.constants import PUBLIC_ACCESS_PREVENTION_ENFORCED
 from google.cloud import dataproc_v1 as dataproc, storage
 
@@ -73,17 +73,20 @@ def create_cluster(project_id, region, cluster_name, num_workers, dataproc_image
         return False
 
 
-def create_bucket(bucket_name, project_id, region, ):
+def create_bucket(bucket_name, project_id, region):
     """Creates a new bucket."""
-    storage_client = storage.Client()
-    bucket = storage_client.create_bucket(bucket_name, project=project_id, location=region)
-    logging.info(f"Bucket {bucket.name} created")
-    bucket.iam_configuration.uniform_bucket_level_access_enabled = True
-    bucket.iam_configuration.public_access_prevention = (
-        PUBLIC_ACCESS_PREVENTION_ENFORCED
-    )
-    bucket.patch()
-    logging.info(f"Patch applied to {bucket.name}")
+    # storage_client = storage.Client()
+    # bucket = storage_client.create_bucket(bucket_name, project=project_id, location=region)
+    #
+    # bucket.iam_configuration.uniform_bucket_level_access_enabled = True
+    # bucket.iam_configuration.public_access_prevention = (
+    #     PUBLIC_ACCESS_PREVENTION_ENFORCED
+    # )
+    # bucket.patch()
+    # logging.info(f"Patch applied to {bucket_name}")
+    os.system(f'gcloud storage buckets create gs://{bucket_name}  --project={project_id} '
+              f'--location={region} --uniform-bucket-level-access --public-access-prevention')
+    logging.info(f"Bucket {bucket_name} created")
 
 
 def delete_bucket(bucket_name):
