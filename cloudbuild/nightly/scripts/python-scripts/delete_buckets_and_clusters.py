0d0a13825329e93e47c26ad00356e85102fc0254
Prashasti Agarwal <prashastia@google.com>
2024-01-27 23:28:31 +0530

    Adds copyright statement.

--- cloudbuild/nightly/scripts/python-scripts/delete_buckets_and_clusters.py
+++ cloudbuild/nightly/scripts/python-scripts/delete_buckets_and_clusters.py
@@ -1,9 +1,23 @@
+# Copyright 2023 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#            http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License
+
 import argparse
 from absl import logging
 from absl import app
 from collections.abc import Sequence
 import os
-from google.cloud import dataproc_v1, storage
+from google.cloud import dataproc_v1
 
 
 def clear_all_dataproc_dependencies(project_id, cluster_name, region):
