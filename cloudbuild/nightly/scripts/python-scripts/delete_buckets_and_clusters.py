8b96a5efc3c6a59bf65360f25b0af00eca82ba29
Prashasti Agarwal <144427971+prashastia@users.noreply.github.com>
2024-02-07 15:47:14 +0530

    Adds script to create the clusters for the nightly e2e jobs. (#80)
    Creates new clusters for all tests.
    1. Small Bounded tests - includes small table read, nested schema read and query test.
    2. Unbounded read- Reading the partitioned table with dynamic insertions 
    3. Large Table Read - Reading a large table of O(100GB’s)
    
    
    The 3 clusters are created concurrently
    
    - Small Bounded Tests can be created in the regions - [asia-east1, asia-east2, asia-southeast1]
    - Large Table Test can be created in the region [asia-northeast1, asia-northeast2, asia-northeast3]
    - Unbounded read test can be created in the region [asia-south1, asia-south2, asia-southeast2]
    
    Process: 
    1. For each region the following steps are attempted:
    - The staging and temp buckets are created in the region
    - Then cluster creation is attempted
        - We wait for the staging and temp buckets to be successfully created as they take a variable amount of time to be created and be available for reading and writing.
        - Then, cluster creation is attempted.
        - In case cluster creation is successful then the region is set to a text file and the cluster name (which is CLUSTER_NAME+”_” + TIMESTAMP)
        - When cluster creation fails the same process is retried for another region in the arrays mentioned.
    2. The cluster is created in one of the 3 regions.
    3. Then, for each of the read tests we read the cluster name and the final region decided. 
    4. The table read is tested.
    5. Then after the success of last tests for each of the clusters (query-read for small table test, large and unbounded read have only one test) All the running jobs on the clusters are canceled, the staging and temp buckets are deleted and the cluster is also deleted.
    
    /gcbrun

---
+++ cloudbuild/nightly/scripts/python-scripts/delete_buckets_and_clusters.py
@@ -0,0 +1,117 @@
+# Copyright 2023 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#            http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License
+
+import argparse
+from absl import logging
+from absl import app
+from collections.abc import Sequence
+import os
+from google.cloud import dataproc_v1
+
+
+def clear_all_dataproc_dependencies(project_id, cluster_name, region):
+    delete_cluster_buckets(project_id, cluster_name, region)
+    delete_cluster(project_id, cluster_name, region)
+
+
+def delete_cluster_buckets(project_id, cluster_name, region):
+    """Function to clean up cluster specific directories from its temporary and staging GCS
+    buckets.
+    Args: project_id: the project id to which the cluster and job belongs. region: Region
+    to which the cluster belongs. cluster_name: Name of the cluster on which the jobs need to be
+    deleted.
+    """
+    logging.info(f'Attempting to delete cluster {cluster_name} buckets')
+    cluster_client = dataproc_v1.ClusterControllerClient(
+        client_options={'api_endpoint': f'{region}-dataproc.googleapis.com:443'}
+    )
+    get_cluster_request = dataproc_v1.GetClusterRequest(
+        project_id=project_id, region=region, cluster_name=cluster_name
+    )
+    cluster_resource = cluster_client.get_cluster(get_cluster_request)
+
+    cluster_id = cluster_resource.cluster_uuid
+    temp_bucket_location = cluster_resource.config.temp_bucket+'/'+cluster_id
+    staging_bucket_location = cluster_resource.config.config_bucket+('/google-cloud-dataproc'
+                                                                     '-metainfo/')+cluster_id
+    delete_bucket(temp_bucket_location)
+    delete_bucket(staging_bucket_location)
+    logging.info(f'Delete all the buckets connected to cluster {cluster_name}.')
+
+
+def delete_bucket(bucket_name):
+    logging.info(f'Attempting to delete the bucket {bucket_name}')
+    os.system(f'gcloud storage rm --recursive gs://{bucket_name}')
+    logging.info(f'Bucket {bucket_name} deleted')
+
+
+def delete_cluster(project_id, cluster_name, region):
+    """Function to delete a dataproc cluster.
+    Args:
+      project_id: the project id to which the cluster and job belongs.
+      region: Region to which the cluster belongs.
+      cluster_name: Name of the cluster on which the jobs need to be deleted.
+    """
+    logging.info(f'Attempting to delete the cluster {cluster_name}')
+    # Create the cluster client.
+    cluster_client = dataproc_v1.ClusterControllerClient(
+        client_options={'api_endpoint': f'{region}-dataproc.googleapis.com:443'}
+    )
+    # Delete the cluster once the job has terminated.
+    operation = cluster_client.delete_cluster(
+        request={
+            'project_id': project_id,
+            'region': region,
+            'cluster_name': cluster_name,
+        }
+    )
+    operation.result()
+    logging.info(f'Cluster {cluster_name} successfully deleted.')
+
+
+def main(argv: Sequence[str]) -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--cluster_name',
+        dest='cluster_name',
+        help='Name of the cluster to be created.',
+        type=str,
+        required=True,
+    )
+    parser.add_argument(
+        '--project_id',
+        dest='project_id',
+        help='Project Id in which the cluster is created.',
+        type=str,
+        required=True,
+    )
+    parser.add_argument(
+        '--region',
+        dest='region',
+        help='Region in which the cluster is present.',
+        type=str,
+        required=True,
+    )
+
+    args = parser.parse_args(argv[1:])
+    # Providing the values.
+    cluster_name = args.cluster_name
+    region = args.region
+    project_id = args.project_id
+
+    clear_all_dataproc_dependencies(project_id, cluster_name, region)
+
+
+if __name__ == '__main__':
+    app.run(main)
