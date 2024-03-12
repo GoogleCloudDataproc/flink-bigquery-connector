9e5db41ab2c5518997d720ce38bb7129df428edd
Prashasti Agarwal <prashastia@google.com>
2024-03-12 14:34:44 +0530

    Adds more tests based on the present implemented datatypes.

--- flink-connector-bigquery/src/test/java/com/google/cloud/flink/bigquery/sink/serializer/AvroToProtoSerializerTest.java
+++ flink-connector-bigquery/src/test/java/com/google/cloud/flink/bigquery/sink/serializer/AvroToProtoSerializerTest.java
@@ -19,6 +19,7 @@ package com.google.cloud.flink.bigquery.sink.serializer;
 import com.google.api.services.bigquery.model.TableFieldSchema;
 import com.google.api.services.bigquery.model.TableSchema;
 import com.google.protobuf.DescriptorProtos.FieldDescriptorProto;
+import com.google.protobuf.Descriptors;
 import com.google.protobuf.Descriptors.Descriptor;
 import com.google.protobuf.Descriptors.DescriptorValidationException;
 import com.google.protobuf.Descriptors.FieldDescriptor;
@@ -625,5 +626,240 @@ public class AvroToProtoSerializerTest {
         assertThat(fieldDescriptor.getName()).isEqualTo("union_in_record");
         assertThat(fieldDescriptor.hasDefaultValue()).isTrue();
         assertThat(fieldDescriptor.getDefaultValue()).isEqualTo(true);
+    }
+
+    private String getRecord(String name) {
+        return "{\"name\": "
+                + "\""
+                + name
+                + "\", "
+                + "\"type\": \"record\", "
+                + "\"fields\": "
+                + "["
+                + "{\"name\": \"value\", \"type\": \"long\"},"
+                + "{\"name\": \"another_value\",\"type\": \"string\"}"
+                + "]"
+                + "}";
+    }
+
+    @Test
+    public void testRecordInRecordSchemaConversion() throws DescriptorValidationException {
+
+        String fieldString =
+                " \"fields\": [\n"
+                        + "   {\"name\": \"record_in_record\", \"type\": {\"name\": \"record_name\", \"type\": \"record\", \"fields\": "
+                        + "[{ \"name\":\"record_field\", \"type\": "
+                        + getRecord("record_inside_record")
+                        + "}]"
+                        + "}}\n"
+                        + " ]\n";
+
+        Schema avroSchema = getAvroSchemaFromFieldString(fieldString);
+        BigQueryProtoSerializer<GenericRecord> avroToProtoSerializer =
+                new AvroToProtoSerializer(avroSchema);
+        Descriptor descriptor = avroToProtoSerializer.getDescriptor();
+
+        FieldDescriptorProto field = descriptor.findFieldByNumber(1).toProto();
+        assertThat(field.getType()).isEqualTo(FieldDescriptorProto.Type.TYPE_MESSAGE);
+        assertThat(field.getName()).isEqualTo("record_in_record");
+        assertThat(field.getNumber()).isEqualTo(1);
+        assertThat(field.getLabel()).isEqualTo(FieldDescriptorProto.Label.LABEL_REQUIRED);
+        assertThat(field.hasTypeName()).isTrue();
+        Descriptors.Descriptor nestedDescriptor =
+                descriptor.findNestedTypeByName(field.getTypeName());
+
+        field = nestedDescriptor.findFieldByNumber(1).toProto();
+        assertThat(field.getType()).isEqualTo(FieldDescriptorProto.Type.TYPE_MESSAGE);
+        assertThat(field.getName()).isEqualTo("record_field");
+        assertThat(field.getNumber()).isEqualTo(1);
+        assertThat(field.getLabel()).isEqualTo(FieldDescriptorProto.Label.LABEL_REQUIRED);
+        assertThat(field.hasTypeName()).isTrue();
+
+        nestedDescriptor = nestedDescriptor.findNestedTypeByName(field.getTypeName());
+        field = nestedDescriptor.findFieldByNumber(1).toProto();
+        assertThat(field.getType()).isEqualTo(FieldDescriptorProto.Type.TYPE_INT64);
+        assertThat(field.getName()).isEqualTo("value");
+        assertThat(field.getNumber()).isEqualTo(1);
+        assertThat(field.getLabel()).isEqualTo(FieldDescriptorProto.Label.LABEL_REQUIRED);
+
+        field = nestedDescriptor.findFieldByNumber(2).toProto();
+        assertThat(field.getType()).isEqualTo(FieldDescriptorProto.Type.TYPE_STRING);
+        assertThat(field.getName()).isEqualTo("another_value");
+        assertThat(field.getNumber()).isEqualTo(2);
+        assertThat(field.getLabel()).isEqualTo(FieldDescriptorProto.Label.LABEL_REQUIRED);
+    }
+
+    @Test
+    public void testRecordOfLogicalTypeSchemaConversion() throws DescriptorValidationException {
+
+        String fieldString =
+                " \"fields\": [\n"
+                        + "{\"name\": \"record_of_logical_type\","
+                        + " \"type\": "
+                        + "{"
+                        + "\"name\": \"record_name\", "
+                        + "\"type\": \"record\","
+                        + " \"fields\": "
+                        + "["
+                        + "   {\"name\": \"ts_micros\", \"type\": {\"type\": \"long\", \"logicalType\": \"timestamp-micros\"}},\n"
+                        + "   {\"name\": \"ts_millis\", \"type\": {\"type\": \"long\", \"logicalType\": \"timestamp-millis\"}},\n"
+                        + "   {\"name\": \"time_micros\", \"type\": {\"type\": \"long\", \"logicalType\": \"time-micros\"}},\n"
+                        + "   {\"name\": \"time_millis\", \"type\": {\"type\": \"int\", \"logicalType\": \"time-millis\"}},\n"
+                        + "   {\"name\": \"lts_micros\", \"type\": {\"type\": \"long\", \"logicalType\": \"local-timestamp-micros\"}},\n"
+                        + "   {\"name\": \"lts_millis\", \"type\": {\"type\": \"long\", \"logicalType\": \"local-timestamp-millis\"}},\n"
+                        + "   {\"name\": \"date\", \"type\": {\"type\": \"int\", \"logicalType\": \"date\"}},\n"
+                        + "   {\"name\": \"decimal\", \"type\": {\"type\": \"bytes\", \"logicalType\": \"decimal\", \"precision\": 4, \"scale\": 2}},\n"
+                        + "   {\"name\": \"uuid\", \"type\": {\"type\": \"string\", \"logicalType\": \"uuid\"}},\n"
+                        + "   {\"name\": \"geography\", \"type\": {\"type\": \"string\", \"logicalType\": \"geography_wkt\"}}\n"
+                        + "]"
+                        + "}"
+                        + "}\n"
+                        + " ]\n";
+
+        Schema avroSchema = getAvroSchemaFromFieldString(fieldString);
+        BigQueryProtoSerializer<GenericRecord> avroToProtoSerializer =
+                new AvroToProtoSerializer(avroSchema);
+        Descriptor descriptor = avroToProtoSerializer.getDescriptor();
+
+        FieldDescriptorProto fieldDescriptorProto = descriptor.findFieldByNumber(1).toProto();
+        assertThat(fieldDescriptorProto.getName()).isEqualTo("record_of_logical_type");
+        assertThat(fieldDescriptorProto.getNumber()).isEqualTo(1);
+        assertThat(fieldDescriptorProto.getLabel())
+                .isEqualTo(FieldDescriptorProto.Label.LABEL_REQUIRED);
+        assertThat(fieldDescriptorProto.getType())
+                .isEqualTo(FieldDescriptorProto.Type.TYPE_MESSAGE);
+        assertThat(fieldDescriptorProto.hasTypeName()).isTrue();
+        descriptor = descriptor.findNestedTypeByName(fieldDescriptorProto.getTypeName());
+
+        assertThat(descriptor.findFieldByNumber(1).toProto())
+                .isEqualTo(
+                        FieldDescriptorProto.newBuilder()
+                                .setType(FieldDescriptorProto.Type.TYPE_INT64)
+                                .setName("ts_micros")
+                                .setNumber(1)
+                                .setLabel(FieldDescriptorProto.Label.LABEL_REQUIRED)
+                                .build());
+
+        assertThat(descriptor.findFieldByNumber(2).toProto())
+                .isEqualTo(
+                        FieldDescriptorProto.newBuilder()
+                                .setType(FieldDescriptorProto.Type.TYPE_INT64)
+                                .setName("ts_millis")
+                                .setNumber(2)
+                                .setLabel(FieldDescriptorProto.Label.LABEL_REQUIRED)
+                                .build());
+
+        assertThat(descriptor.findFieldByNumber(3).toProto())
+                .isEqualTo(
+                        FieldDescriptorProto.newBuilder()
+                                .setType(FieldDescriptorProto.Type.TYPE_STRING)
+                                .setName("time_micros")
+                                .setNumber(3)
+                                .setLabel(FieldDescriptorProto.Label.LABEL_REQUIRED)
+                                .build());
+
+        assertThat(descriptor.findFieldByNumber(4).toProto())
+                .isEqualTo(
+                        FieldDescriptorProto.newBuilder()
+                                .setType(FieldDescriptorProto.Type.TYPE_STRING)
+                                .setName("time_millis")
+                                .setNumber(4)
+                                .setLabel(FieldDescriptorProto.Label.LABEL_REQUIRED)
+                                .build());
+
+        assertThat(descriptor.findFieldByNumber(5).toProto())
+                .isEqualTo(
+                        FieldDescriptorProto.newBuilder()
+                                .setType(FieldDescriptorProto.Type.TYPE_STRING)
+                                .setName("lts_micros")
+                                .setNumber(5)
+                                .setLabel(FieldDescriptorProto.Label.LABEL_REQUIRED)
+                                .build());
+
+        assertThat(descriptor.findFieldByNumber(6).toProto())
+                .isEqualTo(
+                        FieldDescriptorProto.newBuilder()
+                                .setType(FieldDescriptorProto.Type.TYPE_STRING)
+                                .setName("lts_millis")
+                                .setNumber(6)
+                                .setLabel(FieldDescriptorProto.Label.LABEL_REQUIRED)
+                                .build());
+
+        assertThat(descriptor.findFieldByNumber(7).toProto())
+                .isEqualTo(
+                        FieldDescriptorProto.newBuilder()
+                                .setType(FieldDescriptorProto.Type.TYPE_INT32)
+                                .setName("date")
+                                .setNumber(7)
+                                .setLabel(FieldDescriptorProto.Label.LABEL_REQUIRED)
+                                .build());
+
+        assertThat(descriptor.findFieldByNumber(8).toProto())
+                .isEqualTo(
+                        FieldDescriptorProto.newBuilder()
+                                .setType(FieldDescriptorProto.Type.TYPE_BYTES)
+                                .setName("decimal")
+                                .setNumber(8)
+                                .setLabel(FieldDescriptorProto.Label.LABEL_REQUIRED)
+                                .build());
+
+        assertThat(descriptor.findFieldByNumber(9).toProto())
+                .isEqualTo(
+                        FieldDescriptorProto.newBuilder()
+                                .setType(FieldDescriptorProto.Type.TYPE_STRING)
+                                .setName("uuid")
+                                .setNumber(9)
+                                .setLabel(FieldDescriptorProto.Label.LABEL_REQUIRED)
+                                .build());
+
+        assertThat(descriptor.findFieldByNumber(10).toProto())
+                .isEqualTo(
+                        FieldDescriptorProto.newBuilder()
+                                .setType(FieldDescriptorProto.Type.TYPE_STRING)
+                                .setName("geography")
+                                .setNumber(10)
+                                .setLabel(FieldDescriptorProto.Label.LABEL_REQUIRED)
+                                .build());
+    }
+
+    @Test
+    public void testMultipleDatatypeUnionSchemaConversion() {
+        String fieldString =
+                " \"fields\": [\n"
+                        + "{\"name\": \"multiple_type_union\","
+                        + " \"type\":[\"null\", \"string\", \"int\"]"
+                        + "}"
+                        + " ]\n";
+
+        Schema avroSchema = getAvroSchemaFromFieldString(fieldString);
+        IllegalArgumentException exception =
+                assertThrows(
+                        IllegalArgumentException.class,
+                        () -> AvroToProtoSerializer.getDescriptorSchemaFromAvroSchema(avroSchema));
+        assertThat(exception)
+                .hasMessageThat()
+                .contains("Multiple non-null union types are not supported.");
+    }
+
+    @Test
+    public void testDefaultValueSchemaConversion() throws DescriptorValidationException {
+        String fieldString =
+                " \"fields\": [\n"
+                        + "{\"name\": \"long_with_default\", \"type\": [\"long\", \"null\"], \"default\": 100}"
+                        + " ]\n";
+
+        Schema avroSchema = getAvroSchemaFromFieldString(fieldString);
+        BigQueryProtoSerializer<GenericRecord> avroToProtoSerializer =
+                new AvroToProtoSerializer(avroSchema);
+        Descriptor descriptor = avroToProtoSerializer.getDescriptor();
+
+        FieldDescriptorProto fieldDescriptorProto = descriptor.findFieldByNumber(1).toProto();
+        assertThat(fieldDescriptorProto.getName()).isEqualTo("long_with_default");
+        assertThat(fieldDescriptorProto.getNumber()).isEqualTo(1);
+        assertThat(fieldDescriptorProto.getLabel())
+                .isEqualTo(FieldDescriptorProto.Label.LABEL_OPTIONAL);
+        assertThat(fieldDescriptorProto.getType()).isEqualTo(FieldDescriptorProto.Type.TYPE_INT64);
+        assertThat(fieldDescriptorProto.hasDefaultValue()).isTrue();
+        assertThat(fieldDescriptorProto.getDefaultValue()).isEqualTo("100");
     }
 }
