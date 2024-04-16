4bc5f07cc383c5765dab9fed476bd00724d54e8d
Prashasti Agarwal <prashastia@google.com>
2024-04-16 15:37:18 +0530

    Fixes error.

--- cloudbuild/nightly/scripts/unbounded_table_write.sh
+++ cloudbuild/nightly/scripts/unbounded_table_write.sh
@@ -34,7 +34,7 @@ bq update --expiration 3600 "$DATASET_NAME"."$DESTINATION_TABLE_NAME"
 gcloud dataproc jobs submit flink --id "$JOB_ID" --jar="$GCS_JAR_LOCATION" --cluster="$CLUSTER_NAME" --region="$REGION" --properties="$PROPERTIES" --async -- --gcp-source-project "$PROJECT_NAME" --bq-source-dataset "$DATASET_NAME" --bq-source-table "$SOURCE_TABLE_NAME" --mode unbounded --ts-prop "$TS_PROP_NAME" --partition-discovery-interval "$PARTITION_DISCOVERY_INTERVAL" --gcp-dest-project "$PROJECT_NAME" --bq-dest-dataset "$DATASET_NAME" --bq-dest-table "$DESTINATION_TABLE_NAME"
 
 # Dynamically adding the data. This is timed 2.5 min wait for read and 5 min refresh time.
-python3 cloudbuild/nightly/scripts/python-scripts/insert_dynamic_partitions.py -- --project_name "$PROJECT_NAME" --dataset_name "$DATASET_NAME" --table_name "$TABLE_NAME" --refresh_interval "$PARTITION_DISCOVERY_INTERVAL"
+python3 cloudbuild/nightly/scripts/python-scripts/insert_dynamic_partitions.py -- --project_name "$PROJECT_NAME" --dataset_name "$DATASET_NAME" --table_name "$SOURCE_TABLE_NAME" --refresh_interval "$PARTITION_DISCOVERY_INTERVAL"
 
 # Now the Dataproc job will automatically succeed after stipulated time (18 minutes hardcoded).
 # we wait for it to succeed or finish.
