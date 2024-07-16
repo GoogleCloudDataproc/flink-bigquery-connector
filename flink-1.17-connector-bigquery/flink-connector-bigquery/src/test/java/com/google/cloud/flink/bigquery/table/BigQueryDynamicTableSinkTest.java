49d139a6cebc65202328c18b0380502e70b38b8a
Prashasti Agarwal <prashastia@google.com>
2024-07-16 17:04:13 +0530

    Address review comments.

--- flink-1.17-connector-bigquery/flink-connector-bigquery/src/test/java/com/google/cloud/flink/bigquery/table/BigQueryDynamicTableSinkTest.java
+++ flink-1.17-connector-bigquery/flink-connector-bigquery/src/test/java/com/google/cloud/flink/bigquery/table/BigQueryDynamicTableSinkTest.java
@@ -27,6 +27,7 @@ import org.apache.flink.test.junit5.MiniClusterExtension;
 
 import com.google.cloud.flink.bigquery.fakes.StorageClientFaker;
 import com.google.cloud.flink.bigquery.sink.BigQuerySinkConfig;
+import com.google.cloud.flink.bigquery.sink.serializer.BigQuerySchemaProvider;
 import com.google.cloud.flink.bigquery.sink.serializer.RowDataToProtoSerializer;
 import org.apache.avro.Schema;
 import org.junit.jupiter.api.BeforeAll;
@@ -36,6 +37,7 @@ import org.mockito.Mockito;
 
 import java.io.IOException;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 
@@ -91,7 +93,7 @@ public class BigQueryDynamicTableSinkTest {
         BigQueryDynamicTableSink bigQueryDynamicTableSinkCopy =
                 new BigQueryDynamicTableSink(bigQuerySinkConfig, logicalTypeSchema);
 
-        assertEquals(bigQueryDynamicTableSinkCopy, bigQueryDynamicTableSink.copy());
+        assertTrue(checkEquality(bigQueryDynamicTableSinkCopy, bigQueryDynamicTableSink.copy()));
     }
 
     @Test
@@ -118,5 +120,38 @@ public class BigQueryDynamicTableSinkTest {
                 SinkV2Provider.class,
                 bigQueryDynamicTableSink.getSinkRuntimeProvider(
                         Mockito.mock(DynamicTableSink.Context.class)));
+    }
+
+    /**
+     * Method overwritten to check equality, required for testing.
+     *
+     * @param bigQueryDynamicSink1 Object 1.
+     * @param bigQueryDynamicSink2 Target Object to check equality with.
+     * @return True if {@link Object} is equal to current object.
+     */
+    private boolean checkEquality(Object bigQueryDynamicSink1, Object bigQueryDynamicSink2) {
+        if (bigQueryDynamicSink1 == bigQueryDynamicSink2) {
+            return true;
+        }
+        if (bigQueryDynamicSink1.getClass() != bigQueryDynamicSink2.getClass()) {
+            return false;
+        }
+        BigQueryDynamicTableSink bigQueryDynamicTableSink1 =
+                (BigQueryDynamicTableSink) bigQueryDynamicSink1;
+        BigQueryDynamicTableSink bigQueryDynamicTableSink2 =
+                (BigQueryDynamicTableSink) bigQueryDynamicSink2;
+        BigQuerySinkConfig sinkConfig1 = bigQueryDynamicTableSink1.getSinkConfig();
+        BigQuerySinkConfig sinkConfig2 = bigQueryDynamicTableSink2.getSinkConfig();
+
+        if ((bigQueryDynamicTableSink1.getLogicalType()
+                        == bigQueryDynamicTableSink2.getLogicalType())
+                && (sinkConfig1.getConnectOptions() == sinkConfig2.getConnectOptions())
+                && (sinkConfig1.getSerializer() == sinkConfig2.getSerializer())
+                && (sinkConfig1.getDeliveryGuarantee() == sinkConfig2.getDeliveryGuarantee())) {
+            BigQuerySchemaProvider thisSchemaProvider = sinkConfig1.getSchemaProvider();
+            BigQuerySchemaProvider objSchemaProvider = sinkConfig2.getSchemaProvider();
+            return thisSchemaProvider.getAvroSchema().equals(objSchemaProvider.getAvroSchema());
+        }
+        return false;
     }
 }
