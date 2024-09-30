7f0850666fdacce462db0dc2642da7996919a497
Prashasti Agarwal <prashastia@google.com>
2024-09-30 10:24:05 +0530

    throw BigQueryConnector Exception in case of schema mismatch.

--- flink-1.17-connector-bigquery/flink-connector-bigquery/src/main/java/com/google/cloud/flink/bigquery/source/reader/deserializer/AvroDeserializationSchema.java
+++ flink-1.17-connector-bigquery/flink-connector-bigquery/src/main/java/com/google/cloud/flink/bigquery/source/reader/deserializer/AvroDeserializationSchema.java
@@ -20,10 +20,11 @@ import org.apache.flink.annotation.Internal;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.formats.avro.typeutils.GenericRecordAvroTypeInfo;
 
+import com.google.cloud.flink.bigquery.common.exceptions.BigQueryConnectorException;
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericRecord;
-
-import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * A simple Identity de-serialization for pipelines that just want {@link GenericRecord} as response
@@ -34,14 +35,24 @@ public class AvroDeserializationSchema
         implements BigQueryDeserializationSchema<GenericRecord, GenericRecord> {
 
     private final String avroSchemaString;
+    private static final Logger LOG = LoggerFactory.getLogger(AvroDeserializationSchema.class);
 
     public AvroDeserializationSchema(String avroSchemaString) {
         this.avroSchemaString = avroSchemaString;
     }
 
     @Override
-    public GenericRecord deserialize(GenericRecord record) throws IOException {
-        return record;
+    public GenericRecord deserialize(GenericRecord record) throws BigQueryConnectorException {
+        try {
+            return record;
+        } catch (RuntimeException e) {
+            LOG.error(
+                    String.format(
+                            "Error deserializing Avro Generic Record %s to Avro Generic Record.%nError: %s.%nCause:%s ",
+                            record.toString(), e.getMessage(), e.getCause()));
+            throw new BigQueryConnectorException(
+                    "Error in deserializing to Avro Generic Record", e);
+        }
     }
 
     @Override
