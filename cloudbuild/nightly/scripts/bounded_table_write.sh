0c6ad7117b7dc20861cbf99047ce3ad8f42371e0
Shambhavi Shandilya <shashambhavi@google.com>
2024-11-12 16:13:39 +0530

    Modified Unbounded Integration Tests to read from GCS Bucket (#179)
    Unbounded Reads from BQ Table
    * This test is removed from the Integration tests
    
    Unbounded Reads from BQ Table and using the DataStream API Sink
    *
    [FileSystem](https://nightlies.apache.org/flink/flink-docs-master/docs/connectors/datastream/filesystem/)
    connector is used to perform unbounded reads from GCS bucket
    
    Unbounded Reads from BQ Table and using the Table API Sink
    *
    [FileSystem](https://nightlies.apache.org/flink/flink-docs-master/docs/connectors/table/filesystem/)
    connector is used to perform unbounded reads from GCS bucket
    
    **The Integration Tests:**
    1. The original source file is copied from the base bucket and pasted
    onto a temp folder. This temp folder will now be the source for the
    Flink Job.
    2. The destination table is created (hardcoded schema)
    3. New files are added to the temp folder while the tests run at refresh
    interval of 5 minutes (insert_dynamic_files.py)
    4. After 18 minutes, the job is completed and the row counts are
    asserted (assert_table_count)

--- cloudbuild/nightly/scripts/bounded_table_write.sh
+++ cloudbuild/nightly/scripts/bounded_table_write.sh
@@ -20,8 +20,8 @@ IS_EXACTLY_ONCE_ENABLED=$4
 
 # We won't run this async as we can wait for a bounded job to succeed or fail.
 # Create the destination table from the source table schema.
-python3 cloudbuild/nightly/scripts/python-scripts/create_sink_table.py -- --project_name "$PROJECT_NAME" --dataset_name "$DATASET_NAME" --source_table_name "$SOURCE_TABLE_NAME" --destination_table_name "$DESTINATION_TABLE_NAME"
+python3 cloudbuild/nightly/scripts/python-scripts/create_sink_table.py -- --project_name "$PROJECT_NAME" --dataset_name "$DATASET_NAME" --source_table_name "$SOURCE" --destination_table_name "$DESTINATION_TABLE_NAME"
 # Set the expiration time to 1 hour.
 bq update --expiration 3600 "$DATASET_NAME"."$DESTINATION_TABLE_NAME"
 # Run the sink JAR JOB
-gcloud dataproc jobs submit flink --id "$JOB_ID" --jar="$GCS_JAR_LOCATION" --cluster="$CLUSTER_NAME" --region="$REGION" --properties="$PROPERTIES" -- --gcp-source-project "$PROJECT_NAME" --bq-source-dataset "$DATASET_NAME" --bq-source-table "$SOURCE_TABLE_NAME" --gcp-dest-project "$PROJECT_NAME" --bq-dest-dataset "$DATASET_NAME" --bq-dest-table "$DESTINATION_TABLE_NAME" --sink-parallelism "$BOUNDED_JOB_SINK_PARALLELISM" --is-sql "$IS_SQL" --exactly-once "$IS_EXACTLY_ONCE_ENABLED"
+gcloud dataproc jobs submit flink --id "$JOB_ID" --jar="$GCS_JAR_LOCATION" --cluster="$CLUSTER_NAME" --region="$REGION" --properties="$PROPERTIES" -- --gcp-source-project "$PROJECT_NAME" --bq-source-dataset "$DATASET_NAME" --bq-source-table "$SOURCE" --gcp-dest-project "$PROJECT_NAME" --bq-dest-dataset "$DATASET_NAME" --bq-dest-table "$DESTINATION_TABLE_NAME" --sink-parallelism "$BOUNDED_JOB_SINK_PARALLELISM" --is-sql "$IS_SQL" --exactly-once "$IS_EXACTLY_ONCE_ENABLED"
--- cloudbuild/nightly/scripts/python-scripts/assert_table_count.py
+++ cloudbuild/nightly/scripts/python-scripts/assert_table_count.py
@@ -1,15 +1,30 @@
+"""Python script that validates the data written to a BigQuery table by
+the Flink job.
+It compares the total row count and unique key count in the source
+(either a BigQuery table or a GCS URI) with the destination BigQuery table.
+The source is a BigQuery Table for bounded read-write tests and GCS file for
+unbounded read-write tests.
+"""
+
 import argparse
 from collections.abc import Sequence
 
 from absl import app
 from google.cloud import bigquery
+from google.cloud import storage
 from absl import logging
 
 
-def execute_query(client, table_name, query):
+def execute_query(bq_client, table_name, query):
+    """Executes a BigQuery query and returns the result.
+    Args:
+        bq_client: A BigQuery client object.
+        table_name: The name of the BigQuery table.
+        query: The SQL query to execute.
+    """
     logging.info(f"Query: {query}")
     try:
-        job = client.query(query, location='US')
+        job = bq_client.query(query, location='US')
         rows = job.result()  # Start the job and wait for it to complete and get the result.
         for row in rows:
             return row['unique_key_count']
@@ -17,30 +32,83 @@ def execute_query(client, table_name, query):
         raise RuntimeError(f'Could not obtain the count of unique keys from table: {table_name}')
 
 
-def get_unique_key_count(client, project_name, dataset_name, table_name):
+def get_unique_key_count(bq_client, project_name, dataset_name, table_name):
+    """Retrieves the count of distinct unique_key from a BigQuery table.
+    Args:
+        bq_client: A BigQuery client object.
+        project_name: The project ID.
+        dataset_name: The dataset name.
+        table_name: The table name.
+    Returns:
+        The count of distinct unique keys in the table.
+    """
     table_id = f"{project_name}.{dataset_name}.{table_name}"
     query = (
         "SELECT COUNT(DISTINCT(unique_key)) as unique_key_count FROM `" + table_id + "`;"
     )
-    return execute_query(client, table_name, query)
+    return execute_query(bq_client, table_name, query)
 
 
-def get_total_row_count(client, project_name, dataset_name, table_name):
+def get_total_row_count_bigquery(bq_client, project_name, dataset_name, table_name):
+    """Retrieves the total row count from a BigQuery table.
+    Args:
+        bq_client: A BigQuery client object.
+        project_name: The project ID.
+        dataset_name: The dataset name.
+        table_name: The table name.
+    Returns:
+        The total number of rows in the table.
+    """
     table_id = f"{project_name}.{dataset_name}.{table_name}"
     query = (
         "SELECT COUNT(*) as unique_key_count FROM `" + table_id + "`;"
     )
-    return execute_query(client, table_name, query)
+    return execute_query(bq_client, table_name, query)
 
 
-def assert_total_row_count(client, project_name, dataset_name, source_table_name,
-                           destination_table_name, is_exactly_once):
-    source_total_row_count = get_total_row_count(client, project_name, dataset_name,
-                                                 source_table_name)
-    logging.info(f"Total Row Count for Source Table {source_table_name}:"
+def get_total_row_count_gcs_file(storage_client, source_gcs_uri):
+    """Retrieves the total row count from a CSV file in GCS.
+    Args:
+        storage_client: A GCS client object.
+        source_gcs_uri: The GCS URI of the CSV file.
+    Returns:
+        The total number of rows in the CSV file.
+    """
+    path_to_csv = source_gcs_uri + "fullSource.csv"
+    bucket_name = path_to_csv.split("/")[2]
+    blob_path = '/'.join(path_to_csv.split('/')[3:])
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(blob_path)
+    content = blob.download_as_string().decode('utf-8')
+    row_count = len(content.splitlines())
+
+    return row_count
+
+
+def assert_total_row_count(bq_client, storage_client, project_name, dataset_name, source,
+                           destination_table_name, mode, is_exactly_once):
+    """Asserts that the total row count in the source, either GCS bucket or BQ table
+    matches the destination BQ Table.
+    Args:
+        bq_client: A BigQuery client object.
+        storage_client: A GCS client object.
+        project_name: The project ID.
+        dataset_name: The dataset name.
+        source: The source table name or GCS URI.
+        destination_table_name: The destination table name.
+        mode: The mode of the Flink job ('bounded' or 'unbounded').
+        is_exactly_once: True if exactly-once processing is enabled.
+    """
+    source_total_row_count = 0
+    if mode == "unbounded":
+        source_total_row_count = get_total_row_count_gcs_file(storage_client, source)
+    else:
+        source_total_row_count = get_total_row_count_bigquery(bq_client, project_name, dataset_name,
+                                                 source)
+    logging.info(f"Total Row Count for Source {source}:"
                  f" {source_total_row_count}")
 
-    destination_total_row_count = get_total_row_count(client, project_name, dataset_name,
+    destination_total_row_count = get_total_row_count_bigquery(bq_client, project_name, dataset_name,
                                                       destination_table_name)
     logging.info(f"Total Row Count for Destination Table {destination_table_name}:"
                  f" {destination_total_row_count}")
@@ -52,14 +120,32 @@ def assert_total_row_count(client, project_name, dataset_name, source_table_name
             raise AssertionError("Destination Row count is less than Source Row Count")
 
 
-def assert_unique_key_count(client, project_name, dataset_name, source_table_name,
+def assert_unique_key_count(bq_client, storage_client, project_name, dataset_name, source,
                             destination_table_name,
+                            mode,
                             is_exactly_once):
-    source_unique_key_count = get_unique_key_count(client, project_name, dataset_name,
-                                                   source_table_name)
+    """Asserts that the unique key count in the source, either GCS bucket or BQ table
+    matches the destination BQ Table.
+    Args:
+        bq_client: A BigQuery client object.
+        storage_client: A GCS client object.
+        project_name: The project ID.
+        dataset_name: The dataset name.
+        source: The source table name or GCS URI.
+        destination_table_name: The destination table name.
+        mode: The mode of the Flink job ('bounded' or 'unbounded').
+        is_exactly_once: True if exactly-once processing is enabled.
+    """
+    source_unique_key_count = 0
+    # The rows in the source for unbounded mode are unique
+    if mode == "unbounded":
+            source_unique_key_count = get_total_row_count_gcs_file(storage_client, source)
+    else:
+            source_unique_key_count = get_unique_key_count(bq_client, project_name, dataset_name,
+                                                   source)
     logging.info(
-        f"Unique Key Count for Source Table {source_table_name}: {source_unique_key_count}")
-    destination_unique_key_count = get_unique_key_count(client, project_name, dataset_name,
+        f"Unique Key Count for Source {source}: {source_unique_key_count}")
+    destination_unique_key_count = get_unique_key_count(bq_client, project_name, dataset_name,
                                                         destination_table_name)
     logging.info(
         f"Unique Key Count for Destination Table {destination_table_name}:"
@@ -78,7 +164,7 @@ def main(argv: Sequence[str]) -> None:
     parser.add_argument(
         '--project_name',
         dest='project_name',
-        help='Project Id which contains the table to be read.',
+        help='Project Id which contains the table and GCS bucket to be read.',
         type=str,
         default='',
         required=True,
@@ -86,15 +172,15 @@ def main(argv: Sequence[str]) -> None:
     parser.add_argument(
         '--dataset_name',
         dest='dataset_name',
-        help='Dataset Name which contains the table to be read.',
+        help='Dataset Name which contains the BigQuery Tables.',
         type=str,
         default='',
         required=True,
     )
     parser.add_argument(
-        '--source_table_name',
-        dest='source_table_name',
-        help='Table Name of the table which is source for write test.',
+        '--source',
+        dest='source',
+        help='Table Name or GCS URI which is source for write test.',
         type=str,
         default='',
         required=True,
@@ -118,21 +204,32 @@ def main(argv: Sequence[str]) -> None:
         required=False,
     )
 
+    parser.add_argument(
+            '--mode',
+            dest='mode',
+            help='Source Type',
+            type=str,
+            default='',
+            required=True,
+    )
+
     args = parser.parse_args(argv[1:])
 
     # Providing the values.
     project_name = args.project_name
     dataset_name = args.dataset_name
-    source_table_name = args.source_table_name
+    source = args.source
     destination_table_name = args.destination_table_name
     is_exactly_once = args.is_exactly_once
+    mode = args.mode
 
-    client = bigquery.Client(project=project_name)
-    assert_total_row_count(client, project_name, dataset_name, source_table_name,
-                           destination_table_name, is_exactly_once)
+    bq_client = bigquery.Client(project=project_name)
+    storage_client = storage.Client(project=project_name)
+    assert_total_row_count(bq_client, storage_client, project_name, dataset_name, source,
+                           destination_table_name, mode, is_exactly_once)
 
-    assert_unique_key_count(client, project_name, dataset_name, source_table_name,
-                            destination_table_name, is_exactly_once)
+    assert_unique_key_count(bq_client, storage_client, project_name, dataset_name, source,
+                            destination_table_name, mode, is_exactly_once)
 
 
 if __name__ == '__main__':
