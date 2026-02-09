0430544f61ced13a65b3af237cea8a2524c513c8
yalimu-g <yalimu@google.com>
2026-02-08 22:46:34 -0800

    Use cast to append rows

--- flink-2.1-connector-bigquery/flink-connector-bigquery/src/test/java/com/google/cloud/flink/bigquery/fakes/StorageClientFaker.java
+++ flink-2.1-connector-bigquery/flink-connector-bigquery/src/test/java/com/google/cloud/flink/bigquery/fakes/StorageClientFaker.java
@@ -35,6 +35,7 @@ import com.google.cloud.bigquery.storage.v1.CreateReadSessionRequest;
 import com.google.cloud.bigquery.storage.v1.DataFormat;
 import com.google.cloud.bigquery.storage.v1.FinalizeWriteStreamResponse;
 import com.google.cloud.bigquery.storage.v1.FlushRowsResponse;
+import com.google.cloud.bigquery.storage.v1.ProtoRows;
 import com.google.cloud.bigquery.storage.v1.ProtoSchema;
 import com.google.cloud.bigquery.storage.v1.ReadRowsRequest;
 import com.google.cloud.bigquery.storage.v1.ReadRowsResponse;
@@ -396,7 +397,7 @@ public class StorageClientFaker {
 
             public FakeBigQueryStorageWriteClient(AppendRowsResponse appendResponse) {
                 mockedWriter = Mockito.mock(StreamWriter.class);
-                Mockito.when(mockedWriter.append(Mockito.any()))
+                Mockito.when(mockedWriter.append((ProtoRows) Mockito.any()))
                         .thenReturn(ApiFutures.immediateFuture(appendResponse));
                 writeStream = null;
                 flushResponse = null;
@@ -412,7 +413,7 @@ public class StorageClientFaker {
                 // Mockito cannot unbox "any()" for primitive types, throwing the dreaded
                 // NullPointerException. Use primitive variants for argument matching.
                 OngoingStubbing stubbing =
-                        Mockito.when(mockedWriter.append(Mockito.any(), Mockito.anyLong()));
+                        Mockito.when(mockedWriter.append((ProtoRows) Mockito.any()));
 
                 if (appendResponseFutures.length == 0) {
                     stubbing.thenThrow(
